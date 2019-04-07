5c54eb012a51a82b70e168984ff90513cb721a32
richor <ys.song320@gmail.com>
2019-04-07 17:06:47 +0800

    Create wait.sh

---
+++ wait.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#usage: wait.sh pid run.sh
+#used to wait the pid to finish, then submit run.sh
+
+if [ $# == 0  ]; then
+  echo "usage: wait.sh pid run.sh"
+  echo "used to wait the pid to finish, then submit run.sh"
+else 
+  while true; do
+    if ! kill -0 $1 > /dev/null 2>&1; then
+	    echo "PID $1 finished!" >&2
+	    bash $2
+      break
+    else
+	    sleep 120
+    fi
+  done
+fi
