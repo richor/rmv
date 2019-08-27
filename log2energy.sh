940016814eadb4ff12abcecfa3cab19289133df7
richor <ys.song320@gmail.com>
2019-08-27 11:29:07 +0800

    make B20 as variable

--- log2energy.sh
+++ log2energy.sh
@@ -2,14 +2,17 @@
 
 input_log='dha.log'
 output_txt='dha1.txt'
+scan1='B20'
+scan2='A19'
+scan3='D18'
 
 echo "aa" > temp
 grep "SCF Done" $input_log >> temp
 grep "Step number" $input_log > temp1
 echo "Step number   1" >> temp1
 paste temp temp1 |grep "Step number   1" |awk '{if(NR>1) print $5}' > temp2
-grep "!       B20" $input_log  > temp
-grep "!       A19" $input_log > temp1
-grep "!       D18" $input_log  > temp1a
+grep "!       $scan1" $input_log  > temp
+grep "!       $scan2" $input_log > temp1
+grep "!       $scan3" $input_log  > temp1a
 paste temp temp1 temp1a| awk '{if(NR>1) print $3,$10,$17}' > temp3
 paste temp3 temp2 | sort > $output_txt
