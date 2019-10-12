0e9fc2924516c885d92af4c59b5275c3a6d53d01
richor <ys.song320@gmail.com>
2019-10-12 18:30:13 +0800

    fix bug of alias

--- rmv/install.sh
+++ rmv/install.sh
@@ -33,6 +33,6 @@ fi
 # modify the command rm to require confirmation
 echo "do you want to add alias to .bash_profile? [Y/N]"
 read ans
-if [ ans = "Y" ]; then
+if [ $ans = "Y" ]; then
 echo "alias rm='rm -i'" >> ~/.bash_profile
 fi
